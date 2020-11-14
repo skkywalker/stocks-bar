81c61de5139458ec63d63f3bee3f2befc69db1af
skkywalker <lucas@hehl.com.br>
2020-11-14 15:41:32 -0300

    Add boilerplate files

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+   name='StocksBar',
+   version='1.0',
+   description='Bar where drinks follow supply/demand',
+   author='Lucas Hehl',
+   author_email='lucas@hehl.com.br',
+   packages=['StocksBar'],
+   install_requires=['flask'],
+   scripts=[
+        'scripts/server',
+        'scripts/client',
+    ]
+)
